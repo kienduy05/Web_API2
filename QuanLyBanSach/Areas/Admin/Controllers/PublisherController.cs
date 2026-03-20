cfb944ac068e1cb888ea44a3d6435d2b9a7fd18c
Nhật Phạm <nhat872005@gmail.com>
2026-03-20 19:33:31 +0700

    validate lai du lieu

--- QuanLyBanSach/Areas/Admin/Controllers/PublisherController.cs
+++ QuanLyBanSach/Areas/Admin/Controllers/PublisherController.cs
@@ -73,7 +73,11 @@ namespace QuanLyBanSach.Areas.Admin.Controllers
                 TempData["Error"] = "Tên nhà xuất bản không được để trống";
                 return RedirectToAction("Index");
             }
-
+            if (!string.IsNullOrEmpty(model.PublisherPhone) && model.PublisherPhone.Length != 10)
+            {
+                TempData["Error"] = "Số điện thoại phải đúng 10 chữ số";
+                return RedirectToAction("Index");
+            }
             var result = await publisherAPI.Update(model);
 
             if (result)
